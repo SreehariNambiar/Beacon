972427d20a4102779e58dc3721bb7fbe494376c8
Sreehari Rajendra Kumar <sreeharirkumar@gmail.com>
2021-02-27 23:24:37 +0530

    Initial Commit

---
+++ beacon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beacon
+//
+//  Created by Sreehari Nambiar on 27/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beacon/beaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  beaconApp.swift
+//  beacon
+//
+//  Created by Sreehari Nambiar on 27/02/21.
+//
+
+import SwiftUI
+
+@main
+struct beaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
